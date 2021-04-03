d1a2bfa5ca1755e8a89d84606ff76b72bde5dced
Guylaine Rozet <guylainerozet02@gmail.com>
2021-04-03 14:50:44 +0200

    Page de login avec routage des composants

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
